203a745ebd6f95953c81d8a45c16a4950e722038
CCHobbit <Markus_Clephas@t-online.de>
2025-01-21 10:57:20 +0100

    Switch_if_else_counter

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,11 +5,16 @@ public class Cat {
     private String firstName;
     private String furColor;
     private int age;
+    private boolean isFemale;
 
-    public Cat(String firstName, String furColor, int age) {
+    private int counter;
+
+    // constructor
+    public Cat(String firstName, String furColor, int age, boolean isFemale) {
         this.firstName = firstName;
         this.furColor = furColor;
         this.age = age;
+        this.isFemale = isFemale;
     }
 
     /** Getter */
@@ -25,10 +30,37 @@ public class Cat {
     }
 
     public String getAge() {
-        return Integer.toString(age);
-    }
+        if (isFemale) {
+            return checkCompliance();
+            
+        } else {
+            return Integer.toString(age);
+        }
+     }
 
-    
+    // Switch Counter
+     private String checkCompliance(){
+
+        counter++;    
+
+        switch (counter) {
+            case 1:
+                return "This is an inappropriate question!";  // 1st time 
+            
+            case 2:
+                return "I'v told you once!"; // 2nd time
+            
+            case 3:
+                return "Talk to the hand!"; // 3rd time
+            
+            default:
+                return "Wie oft noch du Hund!"; // 4th time++
+                
+        }
+
+
+
+     }
   
 
 
